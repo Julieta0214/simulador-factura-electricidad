d30a3967cfb50087eda96953d933ee45d06575d3
Juliana <julianajj0214@gmail.com>
2025-08-07 17:35:30 -0500

    creacion funcion tarifa base

--- scrip.js
+++ scrip.js
@@ -2,5 +2,11 @@
 
 function consumoMensual(){
     let consumo = Number(prompt("Ingrese el consumo en kwh: "))
-    return 
+    return consumo
+}
+
+
+function tarifaBase (){
+    let tarifa = Number(prompt("Ingrese el valor del kwh: "))
+    return tarifa
 }
