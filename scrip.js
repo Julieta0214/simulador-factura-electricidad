a038d3d64fdd2663ee3597e7560b67f53dd3a0ae
Juliana <julianajj0214@gmail.com>
2025-08-07 17:51:36 -0500

    creacion de funcion flecha que retorne el subtotal * consumo de tarifa

--- scrip.js
+++ scrip.js
@@ -4,14 +4,31 @@ function consumoMensual(){
     let consumo = Number(prompt("Ingrese el consumo en kwh: "))
     return consumo
 }
-
+let consumo = consumoMensual()
 
 function tarifaBase (){
     let tarifa = Number(prompt("Ingrese el valor del kwh: "))
     return tarifa
 }
 
+let tarifa = tarifaBase()
+
 const cargoFijo = function(){
     let cargoFijo = Number(prompt("Ingrese el valor cargo fijo por servicio: "))
     return cargoFijo
 }
+let cargo = cargoFijo()
+
+const cargoIva = function(){
+    let cargoIva = Number(prompt("Ingrese el valor del IVA: "))
+    return cargoIva
+}
+
+Let Iva = valorIva()
+
+const calcularSubtotal = () => {
+    let subto = consumo * tarifa
+    return subto
+}
+
+let subTotal = calcularSubtotal()
